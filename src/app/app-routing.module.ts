7430fbfb002ce624f97b0fb7c52ef399ab4cfb8e
BrooklynP <brooklynpedley@gmail.com>
2019-11-16 16:19:12 +0000

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DrawComponent } from './pages/draw/draw.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: DrawComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/draw/draw.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-draw',
+  templateUrl: './draw.component.html',
+  styleUrls: ['./draw.component.css']
+})
+export class DrawComponent implements OnInit {
+
+  canvas: HTMLCanvasElement;
+  canvasCtx: CanvasRenderingContext2D;
+
+  prevX;
+  prevY;
+
+  currX;
+  currY;
+
+  isMouseDown = false;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.canvas = document.getElementById('canvas') as HTMLCanvasElement;
+
+    this.canvas.width = 800;
+    this.canvas.height = 500;
+
+    this.canvasCtx = this.canvas.getContext('2d');
+
+    console.log(this.canvas);
+
+    this.canvas.addEventListener('mousedown', (evt) => {
+      this.currX = evt.clientX;
+      this.currY = evt.clientY;
+
+      this.canvasCtx.moveTo(this.currX, this.currY);
+      this.canvasCtx.beginPath();
+
+      this.isMouseDown = true;
+    });
+
+
+
+    this.canvas.addEventListener('mouseup', () => {
+      this.isMouseDown = false;
+    });
+
+
+    this.canvas.addEventListener('mousemove', (evt) => {
+      this.currX = evt.clientX;
+      this.currY = evt.clientY;
+      if (this.isMouseDown === true) {
+        this.canvasCtx.lineTo(this.currX, this.currY);
+        this.canvasCtx.stroke();
+      }
+    });
+  }
+
+}
