6eb83567f0eaeffd4cb0de458bd555ed0780fb0d
BrooklynP <brooklynpedley@gmail.com>
2019-11-16 16:55:09 +0000

    You can now change brush size

--- src/app/pages/draw/draw.component.ts
+++ src/app/pages/draw/draw.component.ts
@@ -19,6 +19,7 @@ export class DrawComponent implements OnInit {
   isMouseDown = false;
 
   public CurrentColor = '000000';
+  public CurrentStrokeWidth = 1;
 
   constructor() { }
 
@@ -50,6 +51,8 @@ export class DrawComponent implements OnInit {
       if (this.isMouseDown === true) {
         this.canvasCtx.lineTo(this.currX, this.currY);
         this.canvasCtx.strokeStyle = '#' + this.CurrentColor;
+        this.canvasCtx.lineWidth = this.CurrentStrokeWidth;
+        console.log(this.CurrentStrokeWidth);
         this.canvasCtx.stroke();
       }
     });
